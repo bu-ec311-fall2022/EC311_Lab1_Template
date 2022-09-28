dd1bfbe3362e280a191267c9a95b281fb77e57d8
zaid tahir <zaid.butt.tahir@gmail.com>
2022-09-28 18:19:06 -0400

    Add files via upload

---
+++ add_sub_4_bit/add_sub_4_bit.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+// EC-311 Lab-1 Part-1
+  // The names of the variables are as described in the lab handout
+
+module add_sub_4_bit #
+(
+  parameter WIDTH = 4
+)
+(
+  // The inputs 
+  input wire [WIDTH-1:0]        A_input,
+  input wire [WIDTH-1:0]        B_input,
+  input wire                    M_control_input,
+
+  // The outputs
+  output wire                   V_out,
+  output wire                   C_out,
+  output wire [WIDTH-1:0]       S_output
+
+);
+
+
+
+endmodule
